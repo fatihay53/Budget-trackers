c9d7c1007f62fa0e627cd1cd5a419225e7b601c1
Fatih Ay <f.sultanay@gmail.com>
2021-03-14 16:38:33 -0400

    server file updated with mongo atlas connection

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`==> 🌎  Listening on port ${PORT}. Visit http://localhost:${PORT} in your browser.`);
 });
